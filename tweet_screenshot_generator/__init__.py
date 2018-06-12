da3937bd658f7124856a9cf8420bf163e62c7719
Logan Lennox <loganlennox@protonmail.com>
2018-06-12 14:19:02 -0400

    Create basic "generate tweet" functionality

--- tweet_screenshot_generator/__init__.py
+++ tweet_screenshot_generator/__init__.py
@@ -1,1 +1,10 @@
+import os
 import imgkit
+
+BASEDIR = os.path.dirname(os.path.realpath(__file__))
+USERDIR = os.getcwd()
+
+def generate_tweet():
+    imgkit.from_url(BASEDIR + "/tweet_template/template.html",
+                    USERDIR + "/tweet.png",
+                    options = {"format": "png", "width": 640})
