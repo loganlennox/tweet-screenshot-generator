1a5303758c7a335fd5e93d6971fb6e76061a0e0b
Logan Lennox <loganlennox@protonmail.com>
2018-06-14 12:49:09 -0400

    Add basic user input
    The output of the final png file is now influenced by what the user adds as arguments to the "generate_tweet" function. This is done by reading in a base html file and editing it in memory.

--- tweet_screenshot_generator/__init__.py
+++ tweet_screenshot_generator/__init__.py
@@ -4,7 +4,14 @@ import imgkit
 BASEDIR = os.path.dirname(os.path.realpath(__file__))
 USERDIR = os.getcwd()
 
-def generate_tweet():
-    imgkit.from_url(BASEDIR + "/tweet_template/template.html",
-                    USERDIR + "/tweet.png",
-                    options = {"format": "png", "width": 640})
+def generate_tweet(name, username, body):
+    with open(BASEDIR + "/tweet_template/template.html", "r") as file:
+        data = file.read()
+        data = data.replace("_BASEDIR_", BASEDIR)
+        data = data.replace("_name_", name)
+        data = data.replace("_username_", username)
+        data = data.replace("_body_", body)
+
+    options = {"format": "png", "width": 640}
+
+    imgkit.from_string(data, USERDIR + "/tweet.png", options = options)
